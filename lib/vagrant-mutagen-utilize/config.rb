068bd0b188790cafaa4fb8e1c6ccd9e8a4aea0ca
Ryu Sato <ryu@weseek.co.jp>
2020-07-19 04:41:26 +0900

    remove unused variable

--- lib/vagrant-mutagen-utilize/config.rb
+++ lib/vagrant-mutagen-utilize/config.rb
@@ -3,7 +3,6 @@ require 'vagrant'
 module VagrantPlugins
   module Mutagen
     class Config < Vagrant.plugin('2', :config)
-      attr_accessor :id
       attr_accessor :orchestrate
       attr_accessor :ssh_user_config_path
 
